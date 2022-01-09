524bce3029845a5652e24fc0cff0f5efb55d762e
eeduard2 <eeduardo.mb@gmail.com>
2022-01-08 20:12:33 -0600

    Update Navbar.js
    prueba pirmer commit desde github

--- src/components/navegacion/Navbar.js
+++ src/components/navegacion/Navbar.js
@@ -13,7 +13,7 @@ const navbar = () => {
         </button>
         <div className="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
           <div className="offcanvas-header">
-            <h5 className="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
+            <h5 className="offcanvas-title" id="offcanvasNavbarLabel">Menú</h5>
             <button type="button" className="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
           </div>
           <div className="offcanvas-body">
